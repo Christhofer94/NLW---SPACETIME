786d9e3f9852b23d3369c4ba7b7119fe143c3f9c
Christhofer <christhoferxx@gmail.com>
2023-05-21 17:44:12 -0300

    middleware de autenticação

--- web/src/components/Hero.tsx
+++ web/src/components/Hero.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import nlwLogo from '../assets/nlw-spacetime-logo.svg'
 import Image from 'next/image'
 
@@ -13,12 +14,12 @@ export function Hero() {
           Colecione momentos marcantes da sua jornada e compartilhe (se quiser)
           com todo mundo!
         </p>
-        <a
-          href=""
+        <Link
+          href="/memories/new"
           className="inline-block rounded-full  bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
         >
           CADASTRAR LEMBRANÇA
-        </a>
+        </Link>
       </div>
     </div>
   )
