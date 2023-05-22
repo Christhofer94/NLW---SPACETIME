9ef3cdb5839732c9f4e4e12d924fed87940ef82a
Christhofer <christhoferxx@gmail.com>
2023-05-21 22:49:42 -0300

    rota de uploads de arquivos e instalacao node static

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,30 +1,37 @@
-import 'dotenv/config'
-import fastify from 'fastify'
-import cors from '@fastify/cors'
-import jwt from '@fastify/jwt'
-import { memoriesRoutes } from './routes/memories'
-import { authRoutes } from './routes/auth'
+import "dotenv/config";
+import fastify from "fastify";
+import cors from "@fastify/cors";
+import jwt from "@fastify/jwt";
+import multipart from "@fastify/multipart";
+import { memoriesRoutes } from "./routes/memories";
+import { authRoutes } from "./routes/auth";
+import { uploadRoutes } from "./routes/upload";
+import { resolve } from "node:path";
 
-const app = fastify()
+const app = fastify();
+
+app.register(multipart);
+
+app.register(require("@fastify/static"), {
+  root: resolve(__dirname, "../uploads"),
+  prefix: '/uploads'
+});
 
 app.register(cors, {
-  origin: true,  //O true permite que qualquer front-end possa acessar, no caso de um app de produção, devemos informar a url de qual front-end pode acessar
-})
+  origin: true, //O true permite que qualquer front-end possa acessar, no caso de um app de produção, devemos informar a url de qual front-end pode acessar
+});
 app.register(jwt, {
-  secret: 'spacetime',
-})
+  secret: "spacetime",
+});
 
-app.register(authRoutes)
-app.register(memoriesRoutes)
-
+app.register(authRoutes);
+app.register(uploadRoutes);
+app.register(memoriesRoutes);
 app
   .listen({
     port: 3333,
-    host: '0.0.0.0'
+    host: "0.0.0.0",
   })
   .then(() => {
-    console.log('🚀 HTTP server running on http://localhost:3333')
-  })
-
-
-  
+    console.log("🚀 HTTP server running on http://localhost:3333");
+  });
